96cc6afa065741a9efefe3a40723d101bcf9cf2d
Merin S <merin.ds21@duk.ac.in>
2023-08-22 10:28:27 +0530

    update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import cv2
 import numpy as np
 from glob import glob
-from tqdm import tqdm
 
 # Function to rotate the image
 def rotate_image(image, angle):
@@ -20,42 +19,38 @@ output_folder = r'D:\github\image-text-extraction\rotated_images'
 os.makedirs(output_folder, exist_ok=True)
 
 for img_fn in img_fns:
-    results = pipeline.recognize([img_fn])
-    result = results[0]
-
-    # Extract recognized text from OCR results
-    recognized_text = ""
-    for detection in result:
-        text = detection[0]
-        recognized_text += text + " "
-
-    # Print the complete recognized text for the current image
-    print(f"Recognized text for '{img_fn}': {recognized_text}")
-
-    # Read the image
+    # Read the original image
     image = cv2.imread(img_fn)
-    rotated_angle=151
-    # Rotate the image 30 degrees
-    rotated_image = rotate_image(image, rotated_angle)
 
-    # Save the rotated image
-    output_path = os.path.join(output_folder, os.path.basename(img_fn))
-    cv2.imwrite(output_path, rotated_image)
+    # Initialize the rotation angle to None
+    rotation_angle = None
 
-img_fns = glob(r'D:\github\image-text-extraction\rotated_images\*')
-pipeline = keras_ocr.pipeline.Pipeline()
+    # Try rotating the image at different angles
+    for rotated_angle in range(0, 361):
+        # Rotate the image
+        rotated_image = rotate_image(image, rotated_angle)
 
+        # Perform OCR on the rotated image
+        rotated_results = pipeline.recognize([rotated_image])
+        rotated_result = rotated_results[0]
 
-for img_fn in img_fns:
-    results = pipeline.recognize([img_fn])
-    result = results[0]
+        # Extract recognized text from OCR results
+        recognized_text = ""
+        for detection in rotated_result:
+            text = detection[0]
+            recognized_text += text + " "
 
-    # Extract recognized text from OCR results
-    recognized_text = ""
-    for detection in result:
-        text = detection[0]
-        recognized_text += text + " "
+        # Check if the word "psi" is successfully extracted
+        if "psi" in recognized_text:
+            rotation_angle = rotated_angle
+            break
 
-    # Print the complete recognized text for the current image
-    print(f"Recognized text for '{img_fn}' : {recognized_text}")
-    print("The rotation angle needed to extract word",recognized_text.split(" ")[0],"is",rotated_angle,"degree")
+    if rotation_angle is not None:
+        # Save the rotated image
+        rotated_image = rotate_image(image, rotation_angle)
+        output_path = os.path.join(output_folder, os.path.basename(img_fn))
+        cv2.imwrite(output_path, rotated_image)
+        print(f"Rotated image saved to: {output_path}")
+        print(f"The rotation angle needed to extract the word 'psi' is {rotation_angle} degrees")
+    else:
+        print("The word 'psi' was not successfully extracted in rotated images.")
